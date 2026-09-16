1f84f74bd7eb3e6f573d9f9aab98feb97c3bad86
vorondzava <vorondzava@gmail.com>
2026-09-16 13:19:25 +0300

    Add active queries reporting

--- src/main.py
+++ src/main.py
@@ -4,17 +4,16 @@ from monitor import (
     get_database_size,
     get_active_queries
 )
-from report import print_report
-
+from report import (
+    print_report,
+    print_active_queries
+)
 
 connection = get_connection() #Получаем подключение
 
 active = get_active_connections(connection) #Передаём его в мониторинг
 size = get_database_size(connection)
-
 print_report(active,size)
 
 queries = get_active_queries(connection)
-print()
-print("Active queries:")
-print(queries)
+print_active_queries(queries)
--- src/report.py
+++ src/report.py
@@ -2,8 +2,28 @@ def print_report(active_connections, database_size):
     print("============================")
     print(" PostgreSQL Health Monitor")
     print("============================")
-
     print()
-
     print(f"Active connections: {active_connections}")
     print(f"Database size: {database_size}")
+    print()
+
+def print_active_queries(queries):
+    print("============================")
+    print("Active queries:")
+    print("============================")
+    if not queries:
+        print("No active queries")
+        return
+    
+    for pid, user, sql, query_start in queries:
+
+        if len(sql) > 100:
+            sql = sql[:100] + "..."
+
+
+        print("----------------------------")
+        print(f"PID: ", {pid})
+        print(f"User:", {user})
+        print(f"Query:", {sql})
+        print(f"Started:", {query_start})
+        print()
